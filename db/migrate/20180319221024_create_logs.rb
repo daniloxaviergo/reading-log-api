f3e783fbb76c59b1bf310035efb850c7fd926b14
Danilo <daniloxaviergo@gmail.com>
2018-03-19 19:30:07 -0300

    Add table logs

---
+++ db/migrate/20180319221024_create_logs.rb
@@ -0,0 +1,13 @@
+class CreateLogs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :logs do |t|
+      t.belongs_to :project, index: true
+      t.datetime :data
+      t.integer :start_page
+      t.integer :end_page
+      t.text :note, :text
+
+      t.timestamps
+    end
+  end
+end
