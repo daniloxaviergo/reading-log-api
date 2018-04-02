1ffdf5144f426bf9312215088d9c860496cf792f
Danilo <daniloxaviergo@gmail.com>
2018-04-02 14:11:43 -0300

    Using grouped_log class on stats_log

--- app/classes/v1/stats_log.rb
+++ app/classes/v1/stats_log.rb
@@ -3,12 +3,7 @@ class V1::StatsLog
 
   def initialize(logs)
     first_read   = logs.order(data: :asc).first
-    grouped_logs = logs.order(data: :desc).group_by { |l| l.data.to_date }
-    grouped_logs = grouped_logs.map do |(data, grouped)|
-      resp = grouped.first.to_hash
-      resp[:read_pages] = grouped.map(&:read_pages).flatten.sum
-      resp
-    end
+    grouped_logs = V1::GroupLog.new(logs).by_data
 
     @mean = ::V1::MeanLog.new(grouped_logs, first_read)
     @max  = ::V1::MaxLog.new(grouped_logs)
