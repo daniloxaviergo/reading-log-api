cb1ceeec77db69eb5022804c696e97d89e726a81
Danilo <daniloxaviergo@gmail.com>
2018-04-18 15:12:43 -0300

    Bugfix: Graph speculate actual showing read of today

--- app/controllers/v1/dashboard/echart/speculate_actual_controller.rb
+++ app/controllers/v1/dashboard/echart/speculate_actual_controller.rb
@@ -4,7 +4,7 @@ class V1::Dashboard::Echart::SpeculateActualController < ApplicationController
     logs  = Log.range_data(previous_week)
     wdays = V1::GroupLog.new(logs).by_wday
 
-    hoje, previous_week = (Date.today), (Date.today - 14)
+    hoje, previous_week = (Time.zone.now.utc.end_of_day), (Date.today - 14)
     spec_efec = V1::Dashboard::SpeculateActual.new(previous_week, hoje, wdays)
     echart = ::V1::Dashboard::Echart::SpeculateActual.new(spec_efec.comparative)
 
